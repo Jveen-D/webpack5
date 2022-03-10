c3c81ce63d8bafa17ae9e28d8e8973a22ffa512b
Jveen-D <d85066965@icloud.com>
2022-03-10 20:24:10 +0800

    chore:资源管理-加载css

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@
  * @FilePath: /webpack5/src/index.js
  */
 import _ from "lodash";
+import "./style.css";
+
 function component() {
   const element = document.createElement("div");
 
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-
+  element.classList.add("hello");
   return element;
 }
 
