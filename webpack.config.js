c89232ecfc1e614eaab84b9099c8d3f0947465e7
Jveen-D <d85066965@icloud.com>
2022-03-10 22:34:17 +0800

    chore:管理输出-设置 HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,17 @@
  * @FilePath: /webpack5/webpack.config.js
  */
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "管理输出",
+    }),
+  ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
