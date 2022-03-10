13badaf30fb9a9e2c1764df5bffd6738257f1f37
Jveen-D <d85066965@icloud.com>
2022-03-10 20:29:46 +0800

    chore:资源管理-加载images图像

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@
  * @FilePath: /webpack5/src/index.js
  */
 import _ from "lodash";
+import Happy from "./happy.jpeg";
 import "./style.css";
-
 function component() {
   const element = document.createElement("div");
 
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
+
+  // 将图像添加到我们已经存在的 div 中。
+  const myIcon = new Image();
+  myIcon.src = Happy;
+
+  element.appendChild(myIcon);
   return element;
 }
 
