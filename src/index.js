d10dcb5b0c33234d908005219ea5e11fcc243176
Jveen-D <d85066965@icloud.com>
2022-03-10 20:38:31 +0800

    chore:资源管理-加载images图像

--- src/index.js
+++ src/index.js
@@ -9,14 +9,16 @@ import "./style.css";
 function component() {
   const element = document.createElement("div");
 
+  const child = document.createElement("div");
   // lodash 在当前 script 中使用 import 引入
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
+  child.innerHTML = _.join(["Hello", "webpack"], " ");
+  child.classList.add("hello");
 
   // 将图像添加到我们已经存在的 div 中。
   const myIcon = new Image();
   myIcon.src = Happy;
 
+  element.appendChild(child);
   element.appendChild(myIcon);
   return element;
 }
