3d6491141d0851dc3a5eea2d8685272ca19cb150
Jveen-D <d85066965@icloud.com>
2022-03-11 12:39:59 +0800

    chore:代码分离splitChunks

--- webpack.config.js
+++ webpack.config.js
@@ -11,15 +11,8 @@ module.exports = {
   mode: "development",
   entry: {
     print: "./src/print.js",
-    index: {
-      import: "./src/index.js",
-      dependOn: "shared",
-    },
-    another: {
-      import: "./src/another-module.js",
-      dependOn: "shared",
-    },
-    shared: "lodash",
+    index: "./src/index.js",
+    another: "./src/another-module.js",
   },
   output: {
     filename: "[name].bundle.js",
@@ -38,6 +31,8 @@ module.exports = {
     new WebpackManifestPlugin(options),
   ],
   optimization: {
-    runtimeChunk: "single",
+    splitChunks: {
+      chunks: "all",
+    },
   },
 };
