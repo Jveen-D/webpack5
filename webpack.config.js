687c02ac4cf4a77dcdc2840a978dd88fead9ead2
Jveen-D <d85066965@icloud.com>
2022-03-11 12:26:03 +0800

    chore:开发环境

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,9 @@ module.exports = {
     print: "./src/print.js",
   },
   devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Development",
@@ -24,5 +27,6 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: "/webpack",
   },
 };
