06ba0618c509a6e8bbc0da5a6a737af528f1fccb
duwenjie <d85066965@icloud.com>
2022-03-13 11:18:00 +0800

    chore:缓存-将第三方库(library)（例如 lodash 或 react）提取到单独的 vendor chunk 文件中，是比较推荐的做法，这是因为，它们很少像本地的源代码那样频繁修改。因此通过实现以上步骤，利用 client 的长效缓存机制，命中缓存来消除请求，并减少向 server 获取资源，同时还能保证 client 代码和 server 代码版本一致。

--- webpack.config.js
+++ webpack.config.js
@@ -28,4 +28,16 @@ module.exports = {
     }),
     // new BundleAnalyzerPlugin(),
   ],
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
 };
